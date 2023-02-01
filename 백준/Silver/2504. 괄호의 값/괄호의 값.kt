0248564c2220a32de900827b65004e6ea0fa326b
SY2on <93652182+SY2on@users.noreply.github.com>
2023-02-02 02:01:46 +0900

    [Silver I] Title: 괄호의 값, Time: 84 ms, Memory: 11996 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.kt
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.kt
@@ -4,60 +4,47 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.*
 
+class Token(value:Int, pos:Int){
+    var value = value
+    var pos = pos
+}
+
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
-    var stack = Stack<Char>()
-    val list = br.readLine().toCharArray()
-    var temp = 1
-    var result = 0
-    if((list[0]==')')||(list[0]==']')){
-        result =0
-    }else {
-        for (i in list.indices) {
-            if (list[i] == '(') {
-                stack.push('(')
-                temp *= 2
-            } else if (list[i] == '[') {
-                stack.push('[')
-                temp *= 3
-            } else if (list[i] == ')') {
-                if(stack.empty()||stack.pop()!='('){
-                    result = 0
-                    break
-                }
-                val before = list[i - 1]
-                if (before == '[') {
-                    result = 0
-                    break
-                } else if (before == '(') {
-                    result += temp
-                    temp /= 2
-                } else if ((before == ')') || before == ']') {
-                    temp /= 2
-                }
-            } else if (list[i] == ']') {
-                if(stack.empty()||stack.pop()!='['){
-                    result = 0
-                    break
-                }
-                val before = list[i - 1]
-                if (before == '(') {
-                    result = 0
-                    break
-                } else if (before == '[') {
-                    result += temp
-                    temp /= 3
-                } else if ((before == ')') || before == ']') {
-                    temp /= 3
-                }
+    val input = br.readLine()
+
+    var tokens = Stack<Token>()
+
+    tokens.push(Token(0,-1))
+
+    for(i in input.indices){
+        if(input[i]=='('||input[i]=='['){
+            tokens.push(Token(0,i))
+        }else{
+            val openTag = if(input[i]==')'){'('} else {'['}
+
+            //짝맞추기 실패
+            if(tokens.peek().pos==-1||input[tokens.peek().pos]!=openTag){
+                tokens.clear()
+                break
+            }
+            val token = tokens.pop()
+            val num = if(input[i]==')'){2}else{3}
+            //(), []인 상황이라면
+            if(token.pos+1==i){
+                tokens.peek().value+=num
+            }else{
+                tokens.peek().value+= token.value*num
             }
         }
     }
-    if(!stack.empty()){
-        result=0
+
+    if(tokens.size!=1){
+        bw.write("0")
+    }else{
+        bw.write(tokens.peek().value.toString())
     }
-    bw.write(result.toString())
     bw.close()
 }
