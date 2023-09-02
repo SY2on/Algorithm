4bf552afc09295cde257513793faaeb6d60ccd0a
SY2on <93652182+SY2on@users.noreply.github.com>
2023-09-02 20:47:25 +0900

    [Gold III] Title: 탑 보기, Time: 3716 ms, Memory: 40848 KB -BaekjoonHub

--- 백준/Gold/22866. 탑 보기/탑 보기.java
+++ 백준/Gold/22866. 탑 보기/탑 보기.java
@@ -8,7 +8,7 @@ public class Main {
     static int[] closestIdx;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(br.readLine());
         height = new int[n+1];
         leftCnt = new int[n+1];
@@ -23,13 +23,12 @@ public class Main {
         for(int i=1; i<=n; i++){
             int cnt = leftCnt[i]+rightCnt[i];
             if(cnt==0){
-                bw.write("0"+"\n");
+                sb.append("0"+"\n");
             }else{
-                bw.write(cnt+" "+closestIdx[i]+"\n");
+                sb.append(cnt+" "+closestIdx[i]+"\n");
             }
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
     }
     static void leftDp() {
         for(int i=2; i<=n; i++){
