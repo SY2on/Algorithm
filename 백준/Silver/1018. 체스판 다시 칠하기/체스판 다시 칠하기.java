550abe9cd898d045fc4e5fd72a28980fb6a915a8
SY2on <93652182+SY2on@users.noreply.github.com>
2024-01-13 22:52:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 128 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -4,8 +4,11 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static final boolean WHITE = true;
+    static final boolean BLACK = false;
     static int n, m;
-    static char[][] boards;
+    static boolean[][] boards;
 
     static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
@@ -14,11 +17,12 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        boards = new char[n+1][m+1];
+        boards = new boolean[n+1][m+1];
         for(int i=1; i<=n; i++) {
             String input = br.readLine();
             for(int j=1; j<=m; j++) {
-                boards[i][j] = input.charAt(j-1);
+                if(input.charAt(j-1)=='W') boards[i][j] = WHITE;
+                else boards[i][j] = BLACK;
             }
         }
         for(int i=1; i <= m-7; i++){
@@ -33,10 +37,10 @@ public class Main {
     static int divideAndConqWithStartBlack(int x, int y, int size) {
         if(size == 2){
             int paint=0;
-            if(boards[y][x] != 'B') paint++;
-            if(boards[y][x+1] != 'W') paint++;
-            if(boards[y+1][x] != 'W') paint++;
-            if(boards[y+1][x+1] != 'B') paint++;
+            if(boards[y][x] != BLACK) paint++;
+            if(boards[y][x+1] != WHITE) paint++;
+            if(boards[y+1][x] != WHITE) paint++;
+            if(boards[y+1][x+1] != BLACK) paint++;
             return paint;
         }
         int half = size/2;
@@ -48,10 +52,10 @@ public class Main {
     static int divideAndConqWithStartWhite(int x, int y, int size){
         if(size == 2){
             int paint=0;
-            if(boards[y][x+1] != 'B') paint++;
-            if(boards[y+1][x] != 'B') paint++;
-            if(boards[y+1][x+1] != 'W') paint++;
-            if(boards[y][x] != 'W') paint++;
+            if(boards[y][x+1] != BLACK) paint++;
+            if(boards[y+1][x] != BLACK) paint++;
+            if(boards[y+1][x+1] != WHITE) paint++;
+            if(boards[y][x] != WHITE) paint++;
             return paint;
         }
         int half = size/2;
