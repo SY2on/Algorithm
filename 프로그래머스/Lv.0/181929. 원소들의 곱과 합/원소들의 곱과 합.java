88cac51fcd3dbe7d9fd4dbd78cb4743ffe1c095b
SY2on <93652182+SY2on@users.noreply.github.com>
2024-01-13 13:30:40 +0900

    [level Lv.0] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int mul = 1;
+        int sum = 0;
+        for(int n : num_list){
+            mul*=n;
+            sum+=n;
+        }
+        if(mul<sum*sum) return 1;
+        return 0;
+    }
+}
